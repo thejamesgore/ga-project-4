c0bd0e54fa0c44f5e0bda333a552bbfa3c9890bd
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-11-08 11:13:26 +0000

    added a cart page & to browser router, now add to cart button pushes to cart page

---
+++ frontend/src/common/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Cart() {
+  return (
+    <>
+      <div>THIS IS THE CART PAGE</div>
+      <div>THIS IS THE CART PAGE</div>
+      <div>THIS IS THE CART PAGE</div>
+    </>
+  )
+}
