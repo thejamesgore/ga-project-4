493813c778f6e127c9ef095729e18340d5ca5e03
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-11-11 14:41:54 +0000

    Set permission classes to some auth routes

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User, update_last_login
+
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import serializers
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from .models import Product
@@ -64,7 +66,16 @@ def getProduct(request, pk):
 
 # Will return user data
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
