c91d34bdd5d98d83695cba94f13b04afdfe23119
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-11-13 17:31:55 +0000

    added basics of profile page

--- frontend/src/common/ProfilePage.js
+++ frontend/src/common/ProfilePage.js
@@ -7,13 +7,45 @@ import Loading from '../components/Loading'
 import AlertMessage from '../components/AlertMessage'
 
 export default function ProfilePage({ history }) {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState('')
+  const dispatch = useDispatch()
+
+  const userDetails = useSelector((state) => state.userDetails)
+  const { error, loading, user } = userDetails
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push('/login')
+    } else {
+      if (!user || !user.name) {
+        dispatch(getUserDetails('profile'))
+      }
+    }
+  }, [history, userInfo])
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    if (password != confirmPassword) {
+      setMessage('Passwords do not match')
+    } else {
+      // dispatch(register(name, email, password))
+    }
+    // dispatch(register(name, email, password))
+  }
   return (
     <Row>
       <Col md={3}>
         <h2>Profile</h2>
       </Col>
       <Col md={9}>
-        <h2>Orders</h2>
+        <h2>Order History</h2>
       </Col>
     </Row>
   )
