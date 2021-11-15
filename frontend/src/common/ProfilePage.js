6ea5499a6ef0d8c2126316f222489daab6d7f54b
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-11-15 15:23:50 +0000

    added functionality to profile page

--- frontend/src/common/ProfilePage.js
+++ frontend/src/common/ProfilePage.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import { getUserDetails } from '../redux/actions/userActions'
+import { getUserDetails, updateUserProfile } from '../redux/actions/userActions'
 import Loading from '../components/Loading'
 import AlertMessage from '../components/AlertMessage'
+import { userUpdateProfileReducer } from '../redux/reducers/userReducers'
+import {
+  USER_LOGIN_SUCCESS,
+  USER_UPDATE_PROFILE_RESET,
+} from '../redux/constants/userConstants'
 
 export default function ProfilePage({ history }) {
   const [name, setName] = useState('')
@@ -20,29 +25,89 @@ export default function ProfilePage({ history }) {
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
+  const userUpdateprofile = useSelector((state) => state.userUpdateProfile)
+  const { success } = userUpdateProfileReducer
+
   useEffect(() => {
     if (!userInfo) {
       history.push('/login')
     } else {
-      if (!user || !user.name) {
+      if (!user || !user.name || success) {
+        dispatch({ type: USER_UPDATE_PROFILE_RESET })
         dispatch(getUserDetails('profile'))
+      } else {
+        setName(user.name)
+        setEmail(user.email)
       }
     }
-  }, [history, userInfo])
+  }, [history, userInfo, dispatch, user, success])
 
   const submitForm = (e) => {
     e.preventDefault()
     if (password != confirmPassword) {
       setMessage('Passwords do not match')
     } else {
-      // dispatch(register(name, email, password))
+      dispatch(
+        updateUserProfile({
+          id: user._id,
+          name: name,
+          email: email,
+          password: password,
+        })
+      )
+      setMessage('')
     }
-    // dispatch(register(name, email, password))
   }
   return (
     <Row>
       <Col md={3}>
         <h2>Profile</h2>
+        {message && <AlertMessage variant="danger">{message}</AlertMessage>}
+        {error && <AlertMessage variant="danger">{error}</AlertMessage>}
+        {loading && <Loading />}
+        <Form onSubmit={submitForm}>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="passwordConfirm">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="primary">
+            Update Details
+          </Button>
+        </Form>
       </Col>
       <Col md={9}>
         <h2>Order History</h2>
