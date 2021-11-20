339518b084ef514079dfd841fba9189c524876aa
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-11-20 09:13:12 +0000

    added order endpoint to backend so data can be submitted to database

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,14 +4,14 @@ from django.contrib.auth.hashers import make_password
 
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product
+from .models import Product, Order, OrderItem, ShippingAddress
 from .products import products
-from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken, OrderSerializer
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -109,4 +109,44 @@ def updateUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+    orderItems = data['orderItems']
+    if orderItems and len(orderItems) == 0:
+        return Response({'message': 'No order items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        order = Order.objects.create(
+            user=user,
+            payment_method=data['payment_method'],
+            shippingPrice=data['shippingPrice'],
+            totalPrice=data['totalPrice'],
+        )
+        shipping = ShippingAddress.objects.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postCode=data['shippingAddress']['postcode'],
+            country=data['shippingAddress']['country'],
+        )
+        for item in orderItems:
+            product = Product.objects.get(_id=item['product'])
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=item['qty'],
+                price=item['price'],
+                image=product.image.url,
+            )
+
+        product.countInStock -= item.qty
+        product.save()
+
+    serializer = OrderSerializer(order, many=True)
     return Response(serializer.data)
